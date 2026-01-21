6319bdb1989c396e6316b7cbe1942bdc400ca41f
Patiphat_Prathumwan <68010627@kmitl.ac.th>
2026-01-21 16:48:36 +0700

    1

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -4,12 +4,12 @@ const input = +process.argv[2];
 if (Number.isNaN(input) || input <= 0) {
 //   process.exit()
 }
-for(let i = 0; i <= input; i++) {
+for(let i = 1; i <= input; i++) {
     let result = '';
     for (let k = input - i; k > 0; k--) {
         result += ' '
     }
-    for (let j = i; j > 1; j--) {
+    for (let j = i; j >= 1; j--) {
         result += j
     }
     console.log(result)
