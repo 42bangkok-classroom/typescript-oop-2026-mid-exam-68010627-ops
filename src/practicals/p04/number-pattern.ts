c8cd4dda572d8ece4c3d1afe354ee1294395a56e
Patiphat_Prathumwan <68010627@kmitl.ac.th>
2026-01-21 22:29:46 +0700

    p05

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,8 +1,7 @@
 // Write your code below
-
 const input = +process.argv[2];
 if (Number.isNaN(input) || input <= 0) {
-    // process.exit()
+    process.exit(0);
 }
 for(let i = 1; i <= input; i++) {
     let result = '';
--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -1,5 +1,12 @@
 const comment = "TypeScript is a strongly typed programming language"
 
 function shortenComment(comment: string): string {
-  // Write your code below
+  if (!comment) return "";
+
+  const words = comment.split(" ");
+  const filtered = words.filter(
+    word => word.length >= 5 && word.length <= 10
+  );
+
+  return filtered.join(" ");
 }
