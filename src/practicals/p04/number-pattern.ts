779944f249ee9d9c88d8d8af28a89d8ddde0daa3
Patiphat_Prathumwan <68010627@kmitl.ac.th>
2026-01-21 18:08:08 +0700

    test

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -4,9 +4,9 @@ const input = +process.argv[2];
 if (Number.isNaN(input) || input <= 0) {
     // process.exit()
 }
-for(let i = 0; i <= input; i++) {
+for(let i = 1; i <= input; i++) {
     let result = '';
-    for (let k = input - i; k > 0; k--) {
+    for (let k = input - i - 2; k > 0; k--) {
         result += ' '
     }
     for (let j = i; j > 0; j--) {
