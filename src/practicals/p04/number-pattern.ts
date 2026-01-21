324a6f49c3f91f5fa265110b93c59917ce90a219
Patiphat_Prathumwan <68010627@kmitl.ac.th>
2026-01-21 16:11:42 +0700

    i-hope

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,20 +1,20 @@
 // Write your code below
 
-const input = +process.argv[2];
-if (Number.isNaN(input) || input <= 0) {
-//   process.exit()
-}
-let result = '';
-for(let i = 0; i <= input; i++) {
-    for (let k = input - i; k > 0; k--) {
-        result += ' '
-    }
-    for (let j = i; j > 0; j--) {
-        result += (`${j}`)
-    }
-    result += '\n'
-}
-console.log(result)
+// const input = +process.argv[2];
+// if (Number.isNaN(input) || input <= 0) {
+// //   process.exit()
+// }
+// let result = '';
+// for(let i = 0; i <= input; i++) {
+//     for (let k = input - i; k > 0; k--) {
+//         result += ' '
+//     }
+//     for (let j = i; j > 0; j--) {
+//         result += (`${j}`)
+//     }
+//     result += '\n'
+// }
+// console.log(result)
 
 // const input = +process.argv[2];
 // if (Number.isNaN(input) || input <= 0) {
@@ -43,3 +43,18 @@ console.log(result)
 //   result += '\n'
 // }
 // console.log(result)
+
+const input = +process.argv[2];
+if (Number.isNaN(input) || input <= 0) {
+//   process.exit()
+}
+for(let i = 0; i <= input; i++) {
+    let result = '';
+    for (let k = input - i; k > 0; k--) {
+        result += ' '
+    }
+    for (let j = i; j > 0; j--) {
+        result += (`${j}`)
+    }
+    console.log(result)
+}
