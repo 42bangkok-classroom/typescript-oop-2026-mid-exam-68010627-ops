e1748212b173fec6f560200cff8a199ee3959e98
Patiphat_Prathumwan <68010627@kmitl.ac.th>
2026-01-21 15:58:04 +0700

    please!

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -6,10 +6,13 @@ if (Number.isNaN(input) || input <= 0) {
 }
 let result = '';
 for(let i = 0; i <= input; i++) {
-  for (let j = i; j > 0; j--) {
-    result += j
-  }
-  result += '\n'
+    for (let k = input - i; k > 0; k--) {
+        result += ' '
+    }
+    for (let j = i; j > 0; j--) {
+        result += j
+    }
+    result += '\n'
 }
 console.log(result)
 
@@ -17,49 +20,26 @@ console.log(result)
 // if (Number.isNaN(input) || input <= 0) {
 //   process.exit()
 // }
-// let result = '';
-// for(let i = input; i > 0; i--) {
-//   for (let j = i-1; j > 0; j--) {
-//     result += '*'
-//     result += i
+// for(let i = 0; i < input; i++) {
+//   let result = '';
+//   for (let k = input - i - 1; k > 0; k--) {
+//     result += ' '
 //   }
-//   result += '\n'
+//   for (let j = 0; j < input; j++) {
+//     result += '*'
+//   }
+//   console.log(result)
 // }
-// console.log(result)
 
 // const input = +process.argv[2];
 // if (Number.isNaN(input) || input <= 0) {
 //   process.exit()
 // }
 // let result = '';
-// for(let i = input; i > 0; i--) {
+// for(let i = 0; i <= input; i++) {
 //   for (let j = i; j > 0; j--) {
 //     result += j
 //   }
 //   result += '\n'
 // }
 // console.log(result)
-
-// 54321
-// 4321
-// 321
-// 21
-// 1
-
-// const input = +process.argv[2];
-// if (Number.isNaN(input) || input <= 0) {
-//   process.exit()
-// }
-// for(let i = 0; i < input; i++) {
-//   let result = '';
-//   for (let j = 0; j <= i; j++) {
-//     result += '*'
-//   }
-//   console.log(result)
-// }
-
-// *
-// **
-// ***
-// ****
-// *****
