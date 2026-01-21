69bee8374f99de988639ca96dda30eed2fbce05f
Patiphat_Prathumwan <68010627@kmitl.ac.th>
2026-01-21 15:33:45 +0700

    A

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,17 +1,31 @@
 // Write your code below
+
 const input = +process.argv[2];
 if (Number.isNaN(input) || input <= 0) {
   process.exit()
 }
 let result = '';
-for(let i = input; i > 0; i--) {
-  for (let j = i-1; j > 0; j--) {
-    result += '*'
-    result += i
+for(let i = 0; i <= input; i++) {
+  for (let j = i; j > 0; j--) {
+    result += j
   }
   result += '\n'
 }
 console.log(result)
+
+// const input = +process.argv[2];
+// if (Number.isNaN(input) || input <= 0) {
+//   process.exit()
+// }
+// let result = '';
+// for(let i = input; i > 0; i--) {
+//   for (let j = i-1; j > 0; j--) {
+//     result += '*'
+//     result += i
+//   }
+//   result += '\n'
+// }
+// console.log(result)
 
 // const input = +process.argv[2];
 // if (Number.isNaN(input) || input <= 0) {
